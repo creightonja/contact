4a811784133ac54022ad48619229fc7d37f376a9
Jason Creighton <creightonja@gmail.com>
2015-08-07 09:51:02 -0700

    adding class

---
+++ src/contact.php
@@ -0,0 +1,52 @@
+</?php
+//setting class variables
+class Contact
+{
+  private $name;
+  private $email;
+  private $number;
+
+//creating constructor
+  function __construct($name, $email, $number) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->number = $number;
+  }
+}
+
+//creating setters
+  function setName($new_name){
+    $this->name = $new_name;
+  }
+
+  function setEmail($new_email){
+    $this->email = $new_email;
+  }
+
+  function setNumber($new_number){
+    $this->number = $new_number;
+  }
+
+//creating getters
+  function getName(){
+    return $this->name;
+  }
+
+  function getEmail(){
+    return $this-email;
+  }
+
+  function getNumber(){
+    return $this-number;
+  }
+
+//save method for static data
+  function save(){
+    array_push($_SESSION['list_of_contacts'])
+  }
+
+//get method for static data
+  static function getAll(){
+    return $_SESSION['list_of_contacts']
+  }
+?>
