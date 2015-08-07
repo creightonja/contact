86e88c02c850668c6493d462125709b997d1e504
Jason Creighton <creightonja@gmail.com>
2015-08-07 13:14:29 -0700

    finished contact project

--- app/app.php
+++ app/app.php
@@ -2,41 +2,38 @@
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/contact.php";
 
-//Creating new session
+//creating session save file
   session_start();
-    if (empty($_SESSION['list_of_contacts'])) {
-        $_SESSION['list_of_contacts'] = array();
-    }
+  if (empty($_SESSION['list_of_contacts'])) {
+    $_SESSION['list_of_contacts'] = array();
+  }
 
-//loading silex and twig
+//setting silex and twig paths
   $app = new Silex\Application();
   $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
   ));
 
 
-  //Base directory to redirect to home page.
+//linking to contact form to set and view contacts
   $app->get("/", function() use ($app) {
-    return $app['twig']->render('contact.html.twig', array('contacts' => Contact::getAll()));
+    return $app['twig']->render('contact.html.twig', array('tasks' => Contact::getAll())); //changed tasks.twig
   });
 
 
-  //Posts the user input from add a contact
-  $app->post("/added", function() use ($app) {
-    $newcontact = array();
-    $newcontact = new Contact($_POST['input_name'], $_POST['input_email'],
-                      $_POST['input_number']);
-    $newcontact->save();
-
-    return $app['twig']->render('addedcontact.html.twig', array('addedcontact' => $newcontact));
+//linking to create contact page
+  $app->post("/tasks", function() use ($app) {
+    $contact = new Contact($_POST['name'], $_POST['email'], $_POST['phonenumber']);
+    $contact->save();
+    return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
   });
 
 
+//linking to delete contact page
   $app->post("/delete_contacts", function() use ($app) {
     Contact::deleteAll();
     return $app['twig']->render('delete_contacts.html.twig');
   });
-
 
   return $app;
 ?>
--- src/contact.php
+++ src/contact.php
@@ -1,60 +1,52 @@
 <?php
+  class Contact {
 
-//setting class variables
-class Contact
-{
-  private $name;
-  private $email;
-  private $phone_number;
+    private $name;
+    private $email;
+    private $phonenumber;
+
+//Setting constructor
+    function __construct($name, $email, $phonenumber) {
+      $this->name = $name;
+      $this->email = $email;
+      $this->phonenumber = $phonenumber;
+    }
+
+//Setting setters
+    function setName($new_name) {
+      $this->name = (string) $new_name;
+    }
+    function setEmail($new_email) {
+      $this->email = (string) $new_email;
+    }
+    function setPhonenumber($new_phonenumber) {
+      $this->phonenumber = (string) $new_phonenumber;
+    }
+
+//Setting getters
+    function getName() {
+      return $this->name;
+    }
+    function getEmail() {
+      return $this->email;
+    }
+    function getPhonenumber() {
+      return $this->phonenumber;
+    }
 
 
-//creating constructor
-  function __construct($name, $email, $phone_number) {
-    $this->name = $name;
-    $this->email = $email;
-    $this->phone_number = $phone_number;
+//Setting static methods
+    function save() {  //saving list to cookie
+      array_push($_SESSION['list_of_contacts'], $this);
+    }
+
+    static function getAll() {  //getting list of
+      return $_SESSION['list_of_contacts'];
+    }
+
+    static function deleteAll() {
+      $_SESSION['list_of_contacts'] = array();
+    }
+
   }
-
-//creating setters
-  function setName($new_name){
-    $this->name = (string) $new_name;
-  }
-
-  function setEmail($new_email){
-    $this->email = (string) $new_email;
-  }
-
-  function setNumber($new_phone_number){
-    $this->phone_number = (string) $new_phone_number;
-  }
-
-
-//creating getters
-  function getName(){
-    return $this->name;
-  }
-
-  function getEmail(){
-    return $this->email;
-  }
-
-  function getPhone_number(){
-    return $this->Phone_number;
-  }
-
-
-//save method for static data
-  function save(){
-    array_push($_SESSION['list_of_contacts'], $this);
-  }
-
-//get method for static data
-  static function getAll(){
-    return $_SESSION['list_of_contacts'];
-  }
-
-  static function deleteAll(){
-    $_SESSION['list_of_contacts'] = array();
-  }
-}
 ?>
